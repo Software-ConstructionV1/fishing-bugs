4c62e3cd637477349046054a8ff7c778a57c8ebd
Ibrahim_Salama <ibrahimsalama100@outlook.com>
2025-05-09 16:17:36 +0300

    adding index method to UsersController

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,7 @@ class UsersController < ApplicationController
   def show
     @users = "OSAD 45"
   end
+  def index 
+    @message = "hellow from the other side, I must have called a thousand times to till you i'm working!!!!! :)"
+  end
 end

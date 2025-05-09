dcf0ab7bbaff20f76f624e21b2b6bb5bd07210b3
Rouali <rewanali82@gmail.com>
2025-05-09 17:47:23 +0300

    Fix bug #4: Resolve action_encoding_template error

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,4 @@
-class ApplicationController 
+class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   #Remove the allow_browser line
   # allow_browser versions: :modern

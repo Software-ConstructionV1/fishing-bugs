8dff2d3b61c8d39877fbdb8cfc9872b9dd64404b
omarmorad <omorad5362@gmail.com>
2025-05-09 20:22:46 +0300

    added index Action as it was missing

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,7 @@ class UsersController < ApplicationController
   def show
     @users = "OSAD 45"
   end
+  def index
+    @HelpMeWithThisCrap ="The daaaaamn psych wont insall i dont think its a gem anymore its a damn indian shitty plate " 
+  end
 end
